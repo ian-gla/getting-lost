6070eec72577a7c6ff71be43295251369460c954
Ian Turton <ian.turton@glasgow.ac.uk>
2024-10-16 11:33:35 +0100

    Add additional basemaps for testing - issue #31

--- website/script.js
+++ website/script.js
@@ -1,3 +1,4 @@
+  document.getElementById('map').style.cursor = '' //(reset)
 
 var max_dist = 1; // distance in kn points must be within
 var min_dist = 0; // distance in kn points must be beyond
@@ -42,14 +43,62 @@ var data2_entry = document.querySelector('#more-data');
 
 data_entry.style.display = 'none';
 data2_entry.style.display = 'none';
-var map = L.map("map").setView(center, 17);
-tiles = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-tiles = "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-L.tileLayer(tiles, {
-  maxZoom: 19,
-  attribution:
-  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+});
+
+var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
+
+var osmTopo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+var osmCAT = L.tileLayer('https://tile.openstreetmap.bzh/ca/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="https://www.openstreetmap.cat" target="_blank">Breton OpenStreetMap Team</a>'
+});
+
+var osmBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
+
+var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+});
+
+var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+});
+
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "OpenStreetMap.HOT": osmHOT,
+    "OpenTopo": osmTopo,
+    "OSM CAT": osmCAT,
+  "OSM Bright": osmBright,
+  "Carto Positron": CartoDB_Positron,
+  "Carto Voyager": CartoDB_Voyager,
+
+};
+
+var map = L.map("map",{
+  center: center,
+  zoom: 17,
+  layers: osm, 
+});
+var layerControl = L.control.layers(baseMaps, "", {position:'topleft'}).addTo(map);
 var geocoder = L.Control.geocoder({
   defaultMarkGeocode: false,
   position: 'topleft',
